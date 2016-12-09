880accf807de7e5e651d999fde194f657f19ae60
John Donnelly <jcdonnelly48@gmail.com>
2016-12-09 01:21:05 -0800

    created ruby questions for vampire, made test page directory to tinker with code in

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+puts "Should we order you some garlic bread?"
+g_bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+h_insurance = gets.chomp
+
+puts "#{name}"
+puts "#{age}"
+puts "#{birth_year}"
+puts "#{g_bread}"
+puts "#{h_insurance}"
