8492d4a3a7e8c9d5e7b1a4f9a8c90a3f9b2bf790
John Donnelly <jcdonnelly48@gmail.com>
2016-12-09 05:02:19 -0800

    added a check for suspicious allergies, and age bool

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,7 +5,7 @@ employee_counter = 0
 
 until employee_counter == total_employees
 
-	#Asking new employees questions and assigning them to variables
+	#Asking employees questions and assigning them to variables
 	puts "What is your name?"
 	name = gets.chomp
 	puts "How old are you?"
@@ -21,28 +21,46 @@ until employee_counter == total_employees
 
 	current_year = 2016;
 
+	#Checking if the age is correct, and assigning a boolean 
+	# of true if it is correct, and false if the age doesn't match up
+	correctAge = true;
+	if birth_year + age != current_year
+		correctAge = false;
+	end
+
 	#Setting up the results variable, which can change throughout the statements
 	results = "Results inconclusive";
 
-	if birth_year + age == current_year
+	if correctAge && (g_bread == "yes" || h_insurance == "yes")
 		results = "Probably not a vampire"
-	else
-		ageB = false
 	end
 
-	if ageB == false && (g_bread == "no" || h_insurance == "no")
+	if correctAge == false && (g_bread == "no" || h_insurance == "no")
 		results = "Probably a vampire"
 	end
 
-	if ageB == false && g_bread == "no" && h_insurance == "no"
+	if correctAge == false && g_bread == "no" && h_insurance == "no"
 		results = "Almost certainly a vampire."
-		otherwise_var = true;
 	end
 
 	if name == "Drake Cula" || name == "Tu Fang"
 		results = "Definitely a vampire."
-		otherwise_var = true;
 	end
+
+	#Loop checking if sunshine is an allergy, and if so changing result 
+	#to Probably a vampire.
+	puts "Name your allergies, one at a time, and type \"done\" when finished:"
+	allergy_bool = true
+	while allergy_bool
+		allergy = gets.chomp
+		if allergy == "sunshine"
+			results = "Probably a vampire."
+			allergy_bool = false;
+		elsif allergy == "done"
+			allergy_bool = false;
+		end
+	end
+
 
 	puts "#{results}"
 	employee_counter += 1
