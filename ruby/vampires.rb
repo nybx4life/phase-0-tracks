ae95db0fb39f7284ea80aa057e558f43e293828a
John Donnelly <jcdonnelly48@gmail.com>
2016-12-09 03:14:57 -0800

    finished all of release 2, tested all possibilites

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,16 +1,40 @@
+#Asking the questions and assigning the results to variables.
 puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
 age = gets.chomp
+age = age.to_i
 puts "What year were you born?"
 birth_year = gets.chomp
+birth_year = birth_year.to_i
 puts "Should we order you some garlic bread?"
 g_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 h_insurance = gets.chomp
 
-puts "#{name}"
-puts "#{age}"
-puts "#{birth_year}"
-puts "#{g_bread}"
-puts "#{h_insurance}"
+current_year = 2016;
+
+#Setting up the results variable, which can change throughout the statements
+results = "Results inconclusive";
+
+if birth_year + age == current_year
+	results = "Probably not a vampire"
+else
+	ageB = false
+end
+
+if ageB == false && (g_bread == "no" || h_insurance == "no")
+	results = "Probably a vampire"
+end
+
+if ageB == false && g_bread == "no" && h_insurance == "no"
+	results = "Almost certainly a vampire."
+	otherwise_var = true;
+end
+
+if name == "Drake Cula" || name == "Tu Fang"
+	results = "Definitely a vampire."
+	otherwise_var = true;
+end
+
+puts "#{results}"
