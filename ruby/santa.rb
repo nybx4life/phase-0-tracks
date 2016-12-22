df0873381564ba85bf299f96f20c7413bd3cafda
John Donnelly <jcdonnelly48@gmail.com>
2016-12-22 00:54:24 -0800

    added diverse initializations & tested them with driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,15 +3,40 @@ class Santa
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
+	def introduction
+		puts "Hello, I'm a Santa instance, my gender is #{@gender}, and my ethnicity is #{@ethnicity}"
+	end
+
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type}!"
 	end
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reinder_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", 
+			"Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 end
 
-bad_santa = Santa.new
-bad_santa.speak
-bad_santa.eat_milk_and_cookies("oreo")
+# bad_santa = Santa.new
+# bad_santa.speak
+# bad_santa.eat_milk_and_cookies("oreo")
+
+santas = []
+genders_array = ["male", "female", "fendergender", "blendergender", "nogender", "neutragender",
+	"herbagender", "sendergender"]
+ethnicity_array = ["elephant-man", "asian", "Middle Eastern", "black", "white", 
+	"Eastern European", "Mexican-American", "N/A"]
+
+genders_array.length.times do |counter|
+	santas << Santa.new(genders_array[counter], ethnicity_array[counter])
+end
+
+santas.each do |santa_instances|
+	santa_instances.speak
+	santa_instances.introduction
+end
+
